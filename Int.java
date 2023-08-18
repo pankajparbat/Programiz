0c644b27838590ef01435aaf6ecd9cd50bede59e
Pankaj Dashrath Parbat <121680162+pankajparbat@users.noreply.github.com>
2023-08-18 17:57:25 +0530

    Add files via upload

---
+++ Int.java
@@ -0,0 +1,12 @@
+package programizexample;
+
+import java.util.Scanner;
+
+public class Int {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter teh number");
+        int num= sc.nextInt();
+        System.out.println("Entered number is "+num);
+    }
+}
