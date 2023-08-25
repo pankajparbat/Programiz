a336bbdaf94950c59b5c14b155df2b13d186574c
Pankaj Dashrath Parbat <121680162+pankajparbat@users.noreply.github.com>
2023-08-25 18:10:10 +0530

    Add files via upload
    Java Program to Display Alphabets (A to Z) using loop

---
+++ display.java
@@ -0,0 +1,10 @@
+package every_day;
+
+public class display {
+    public static void main(String[] args) {
+    for (char i='A';i<='Z';i++)
+    {
+        System.out.print(i+" ");
+    }
+    }
+}
